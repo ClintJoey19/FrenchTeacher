9da07771cfa8d4dd630425caba09e5bae6838385
ClintJoey19 <llosalaclintjoey@gmail.com>
2024-01-11 21:44:46 +0800

    project finished

--- app/src/main/java/com/cj/frenchteacher/MainActivity.java
+++ app/src/main/java/com/cj/frenchteacher/MainActivity.java
@@ -2,13 +2,62 @@ package com.cj.frenchteacher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button black, green, purple, red, yellow;
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        black = findViewById(R.id.black);
+        green = findViewById(R.id.green);
+        purple = findViewById(R.id.purple);
+        red = findViewById(R.id.red);
+        yellow = findViewById(R.id.yellow);
+        black.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.black);
+                mediaPlayer.start();
+            }
+        });
+
+        green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.green);
+                mediaPlayer.start();
+            }
+        });
+
+        purple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.purple);
+                mediaPlayer.start();
+            }
+        });
+
+        red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.red);
+                mediaPlayer.start();
+            }
+        });
+
+        yellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.yellow);
+                mediaPlayer.start();
+            }
+        });
     }
 }
